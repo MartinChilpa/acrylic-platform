5ca01247c54d095ff6714571bba013606c593ea3
Nazim Sumra <nazimsumra2778@gmail.com>
2024-04-19 01:17:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/acrylic-app'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/acrylic-app/index.html'));});
-app.listen(process.env.PORT || 8080);
+
+// Serve static files from the 'dist/acrylic-app' directory
+app.use(express.static(path.join(__dirname, 'dist', 'acrylic-app', 'browser')));
+
+// For any other route, serve the 'index.html' file
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'acrylic-app', 'browser', 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
